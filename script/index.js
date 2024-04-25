784e841fb5700bd2dca91959f0d1cbd61f6d721b
Galahadvn098 <phingtodeptrai098@gmail.com>
2024-04-25 16:24:12 +0700

    1111

--- script/index.js
+++ script/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 // sidebar
 function showSidebar(){
@@ -8,15 +7,4 @@ function showSidebar(){
   function hideSidebar(){
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none'
-=======
-
-// sidebar
-function showSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'flex'
-  }
-  function hideSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'none'
->>>>>>> ad8716bffe265fb05a9aed55247e144bb7afa50c
   }
